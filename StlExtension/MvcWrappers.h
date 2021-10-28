ae5961b43eb900666709b575d0f1e570d76ad8fc
Dima Truhsin <trushindima@yandex.ru>
2021-10-28 17:20:23 +0300

    Add MVC wrappers

---
+++ StlExtension/MvcWrappers.h
@@ -0,0 +1,56 @@
+#ifndef MVCWRAPPERS_H
+#define MVCWRAPPERS_H
+
+#include <memory>
+
+namespace NSLibrary {
+
+template<class TImpl>
+class CBasicWrapper {
+public:
+  using CImplementation = TImpl;
+  CBasicWrapper() : Impl_(std::make_unique<TImpl>()) {
+  }
+  template<class... TArgs>
+  CBasicWrapper(TArgs&&... args)
+      : Impl_(std::make_unique<TImpl>(std::forward<TArgs>(args)...)) {
+  }
+
+  TImpl* operator->() const {
+    return Impl_.get();
+  }
+
+protected:
+  std::unique_ptr<TImpl> Impl_;
+};
+
+template<class TImpl>
+class CModelWrapper : public CBasicWrapper<TImpl> {
+  using CBase = CBasicWrapper<TImpl>;
+
+public:
+  using CBase::CBase;
+  TImpl* model() const {
+    return CBase::Impl_.get();
+  }
+};
+
+template<class TImpl>
+class CViewWrapper : public CBasicWrapper<TImpl> {
+  using CBase = CBasicWrapper<TImpl>;
+
+public:
+  using CBase::CBase;
+};
+
+template<class TImpl>
+class CControllerWrapper : public CBasicWrapper<TImpl> {
+  using CBase = CBasicWrapper<TImpl>;
+
+public:
+  CControllerWrapper() = delete;
+  using CBase::CBase;
+};
+
+} // namespace NSLibrary
+#endif // MVCWRAPPERS_H
